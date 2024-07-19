bfefa3493545d5d1faf51acd05c315cc847fae89
aditya-varma8019 <rvenkataditya@gmail.com>
2024-07-19 23:46:02 +0530

    frontend category CRUD

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/auth';
+// import 'antd/dist/antd.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
