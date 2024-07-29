c8388ddbee32af7feb915fa0885b8e168d2926ba
aditya-varma8019 <rvenkataditya@gmail.com>
2024-07-29 22:54:38 +0530

    update

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
-app.use(cors())
+app.use(cors(
+    { origin: 'https://ecommerce-app-frontend-two.vercel.app' }
+))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, './client/build')))
