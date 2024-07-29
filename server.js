a7d45055523c74bb693665c608608e92f3ccbb72
aditya-varma8019 <rvenkataditya@gmail.com>
2024-07-29 22:35:42 +0530

    removed build

--- server.js
+++ server.js
@@ -19,13 +19,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
-app.use(cors(
-    {
-        origin: ["https://ecommerce-app-frontend-azure.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, './client/build')))
