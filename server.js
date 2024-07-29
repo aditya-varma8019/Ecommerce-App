8e4fb3563abef28fd73d9847097b7a4ef6b97751
aditya-varma8019 <rvenkataditya@gmail.com>
2024-07-29 21:41:56 +0530

    s

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 connectDb();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
