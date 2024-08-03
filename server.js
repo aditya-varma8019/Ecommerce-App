690621cf88dc5954b6f70840fa4fcb2662ffb20d
aditya-varma8019 <rvenkataditya@gmail.com>
2024-08-03 14:55:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,21 @@ connectDb();
 
 dotenv.config();
 
-app.use(cors(
-    {
-        origin: ['http://localhost:3000', 'https://ecommerce-app-5dgy.onrender.com/']
-        // credentials: true
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://ecommerce-app-5dgy.onrender.com'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
     }
-))
+}));
 app.use(express.json());
 app.use(morgan('dev'))
 // app.use(express.static(path.join(__dirname, './client/build')))
