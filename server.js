5df60b61b16d419b6b16972e136ac7bd0cef4ed8
aditya-varma8019 <rvenkataditya@gmail.com>
2024-08-03 14:44:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,21 +15,15 @@ const app = express();
 connectDb();
 
 // const __filename = fileURLToPath(import.meta.url);
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/client/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
-}
 dotenv.config();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ['http://localhost:3000', 'https://ecommerce-app-5dgy.onrender.com/']
+        // credentials: true
+    }
+))
 app.use(express.json());
 app.use(morgan('dev'))
 // app.use(express.static(path.join(__dirname, './client/build')))
@@ -42,6 +36,19 @@ app.use(morgan('dev'))
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
